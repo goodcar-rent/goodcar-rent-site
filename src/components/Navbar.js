84eb05a67887c9d68e0944eb9a832b2b2eb5143d
deksden <deksden@deksden.com>
2018-08-15 15:48:56 +0700

    - (enh) test some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,16 +10,16 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="GoodCar.rent" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
-          About
+          О компании
         </Link>
         <Link className="navbar-item" to="/products">
-          Products
+          Аренда
         </Link>
       </div>
       <div className="navbar-end">
