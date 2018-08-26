4ca99d94e868efe003c52aaa0abac57d718c26c8
deksden@air <deksden@gmail.com>
2018-08-27 00:13:54 +0700

    Merge branch 'beta'
    # Conflicts:
    #	src/components/card.js
    #	src/components/header.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: '(beta) GoodCar.rent - Прокат машин в Новосибирске, посуточная аренда автомобилей',
+    title: 'GoodCar.rent - Прокат машин в Новосибирске, посуточная аренда автомобилей',
     tel: '+79513888888',
     tel_formatted: '+7 (951) 388-88-88',
     email: 'office@goodcar.rent',
