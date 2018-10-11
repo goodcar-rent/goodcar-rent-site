de6aacdbba8a4e9b0e7217be843351fe3d91739a
deksden@air <deksden@gmail.com>
2018-10-12 01:15:44 +0700

    - (enh) google analytics installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,23 @@ module.exports = {
         version: 2,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-23824119-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-typography`,
