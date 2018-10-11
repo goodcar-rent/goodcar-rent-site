070b42d55d3eaf74a87186cf5f7857c6b995052d
deksden@air <deksden@gmail.com>
2018-10-12 01:33:35 +0700

    - (enh) google analytics rplaced with more modern gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,20 +21,12 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
+        // your google analytics tracking id
         trackingId: "UA-23824119-2",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10
+        // enable ip anonymization
+        anonymize: false
       },
     },
     'gatsby-plugin-react-helmet',
