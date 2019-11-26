a8c715a2b5f99d45e732c293e4abdd4ff2d13c43
deksden <deksden@deksden.com>
2019-11-26 11:01:49 +0700

    feature: env flag to turn tel click tracking on and off

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,19 @@ import React from 'react'
 import './header.css'
 import logo from '../images/logo.svg'
 
+
 export function Header (props) {
+  const useTelTracking = (process.env.GATSBY_USE_TEL_TRACKING === 'true') || false
+
   function handleclick(e) {
     e.preventDefault()
     const shortTel = document.getElementById('shortTel')
     const fullTel = document.getElementById('fullTel')
 
-    if(fullTel.style.display === 'none') {
+    if (!useTelTracking) {
+      fullTel.style.display = 'block'
+      shortTel.style.display = 'none'
+    } else if(fullTel.style.display === 'none') {
       fullTel.style.display = 'block'
       shortTel.style.display = 'none'
       window.yaCounter.reachGoal('TELCLICKED')
@@ -29,19 +35,20 @@ export function Header (props) {
             </a>
           </div>
           <div className="top_line_right">
-            <div className="description">Аренда машины в Новосибирске:</div>
+            <div className="description">Аренда авто  в Новосибирске:</div>
             <div className="phone">
               <a
                 id="shortTel"
                 href="./"
                 target="_self"
+                style={ useTelTracking ? {} : { display: 'none' }}
                 onClick={handleclick}
                 title="Позвонить">
                 +7 (951) 388-... (показать)
               </a>
               <a
                 id="fullTel"
-                style={{ display:'none' }}
+                style={ useTelTracking ? { display:'none' }: {}}
                 href={`tel:${props.siteMeta.tel}`}
                 target="_self"
                 title="Позвонить">
