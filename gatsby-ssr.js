1d6d6cd5c23a208cdbb02cca155ce577c22920f3
deksden <deksden@deksden.com>
2019-12-24 18:15:15 +0700

    feature: properly add scripts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -184,59 +184,10 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
     />,
   ])
   setPostBodyComponents([
-    <script
-      key="fb_pixel"
+    <script key="yametrika"
       type="text/javascript"
       dangerouslySetInnerHTML={{
         __html: `
-        <!-- Facebook Pixel Code -->
-          <script>
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.defer=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1273628269511577');
-            fbq('track', 'PageView');
-          </script>
-          <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=1273628269511577&ev=PageView&noscript=1"
-          /></noscript>
-          <!-- End Facebook Pixel Code -->
-        `,
-      }}
-    />,
-  ])
-  setPostBodyComponents([
-    <script
-      key="ga"
-      type="text/javascript"
-      dangerouslySetInnerHTML={{
-        __html: `
-          <!-- Global site tag (gtag.js) - Google Analytics -->
-          <script defer src="https://www.googletagmanager.com/gtag/js?id=UA-23824119-2"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'UA-23824119-2');
-          </script>        
-        `,
-      }}
-    />,
-  ])
-  setPostBodyComponents([
-    <script
-      key="ga"
-      type="text/javascript"
-      dangerouslySetInnerHTML={{
-        __html: `
-          <!-- Yandex.Metrika counter -->
-          <script type="text/javascript" >
              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.defer=1,k.src=r,a.parentNode.insertBefore(k,a)})
              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -245,12 +196,57 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
                   clickmap:true,
                   trackLinks:true,
                   accurateTrackBounce:true,
-                  webvisor:flase
+                  webvisor:false
              });
-          </script>
-          <noscript><div><img src="https://mc.yandex.ru/watch/50703043" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-          <!-- /Yandex.Metrika counter -->        
-        `,
+         `,
+      }}
+    />,
+    <noscript key="noscript-yametrika">
+      <div>
+        <img
+          src='https://mc.yandex.ru/watch/50703043'
+          style={{ position: "absolute", left: "-9999px" }}
+        />
+      </div>
+    </noscript>,
+    <script key="fbpixel"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.defer=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1273628269511577');
+          fbq('track', 'PageView');
+         `,
+      }}
+    />,
+    <noscript key="noscript-fbpixel">
+      <img
+        src='https://www.facebook.com/tr?id=1273628269511577&ev=PageView&noscript=1'
+        height={1} width={1}
+        style={{ display: "none" }}
+      />
+    </noscript>,
+    <script key="ga"
+      type="text/javascript"
+      src="https://www.googletagmanager.com/gtag/js?id=UA-23824119-2"
+      defer
+    />,
+    <script key="ga-init"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-23824119-2'
+         `,
       }}
     />,
   ])
