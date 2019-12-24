1886d8152325c192099cbeec6f01f25e6d576f5d
deksden <deksden@deksden.com>
2019-12-24 11:51:56 +0700

    feat: add scripts via SSR API

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,7 +7,7 @@
 // You can delete this file if you're not using it
 
 import React from 'react'
-export const onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
   setHeadComponents([
     <script
       key="modernizr"
@@ -180,6 +180,77 @@ export const onRenderBody = ({ setHeadComponents }) => {
               for (var p = 0; p < Modernizr._q.length; p++) Modernizr._q[p]()
               e.Modernizr = Modernizr
             })(window, document)`,
+      }}
+    />,
+  ])
+  setPostBodyComponents([
+    <script
+      key="fb_pixel"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+        <!-- Facebook Pixel Code -->
+          <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.defer=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1273628269511577');
+            fbq('track', 'PageView');
+          </script>
+          <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1273628269511577&ev=PageView&noscript=1"
+          /></noscript>
+          <!-- End Facebook Pixel Code -->
+        `,
+      }}
+    />,
+  ])
+  setPostBodyComponents([
+    <script
+      key="ga"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script defer src="https://www.googletagmanager.com/gtag/js?id=UA-23824119-2"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'UA-23824119-2');
+          </script>        
+        `,
+      }}
+    />,
+  ])
+  setPostBodyComponents([
+    <script
+      key="ga"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          <!-- Yandex.Metrika counter -->
+          <script type="text/javascript" >
+             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.defer=1,k.src=r,a.parentNode.insertBefore(k,a)})
+             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          
+             ym(50703043, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:flase
+             });
+          </script>
+          <noscript><div><img src="https://mc.yandex.ru/watch/50703043" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+          <!-- /Yandex.Metrika counter -->        
+        `,
       }}
     />,
   ])
