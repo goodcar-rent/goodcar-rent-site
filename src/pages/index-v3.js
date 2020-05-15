9e1dcd97e744e5c46381401b339c3013c736b1de
deksden <deksden@deksden.com>
2020-05-15 20:42:30 +0700

    feat: added footer component - WIP

--- src/pages/index-v3.js
+++ src/pages/index-v3.js
@@ -1,7 +1,7 @@
 import React from 'react'
 //import { Link } from 'gatsby'
 // import { graphql } from 'gatsby'
-import { HeaderV3, Advantages, Flamp } from "../components"
+import { HeaderV3, Advantages, Flamp, Footer } from "../components";
 // import Layout from '../components/layout'
 // import { Header } from '../components/header'
 // import { Hero } from '../components/hero'
@@ -20,6 +20,7 @@ const IndexV3Page = () => {
           <HeaderV3 />
           <Advantages />
           <Flamp />
+          <Footer />
       </div>
     )
 }
