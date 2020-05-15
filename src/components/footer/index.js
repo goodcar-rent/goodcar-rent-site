948820450372479742d68993e09202821c22185b
deksden <deksden@deksden.com>
2020-05-15 20:42:30 +0700

    feat: added footer component - WIP

---
+++ src/components/footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import css from './index.module.css'
+
+export const Footer = () => {
+  return (
+    <div className={css.wrapper}>
+    </div>
+  )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './header-v3'
 export * from './advantages'
 export * from './reviews'
+export * from './footer'
